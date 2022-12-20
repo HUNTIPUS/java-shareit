cb7e3a3f457ab68e37da1223c4c64cf57300dbf9
HUNTIPUS <zhigulenok18@yandex.ru>
2022-12-20 16:17:42 +0300

    feat: 1 этап

---
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoInput.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.request.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.user.valid.Create;
+
+import javax.validation.constraints.NotBlank;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Data
+@NoArgsConstructor
+public class ItemRequestDtoInput {
+
+    private Long id;
+    private Long requestorId;
+    @NotBlank(groups = {Create.class})
+    private String description;
+    private LocalDateTime created = LocalDateTime.now(Clock.system(ZoneId.of("Europe/Moscow")));
+}
