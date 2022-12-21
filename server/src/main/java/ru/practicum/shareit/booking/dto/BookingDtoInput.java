3099bd498c66b5aa12b1701035f01d3eb944e731
HUNTIPUS <zhigulenok18@yandex.ru>
2022-12-21 22:32:18 +0300

    feat: 3 этап

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoInput.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoInput.java
@@ -2,11 +2,7 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.user.valid.Create;
 
-import javax.validation.constraints.Future;
-import javax.validation.constraints.FutureOrPresent;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -14,11 +10,7 @@ import java.time.LocalDateTime;
 public class BookingDtoInput {
 
     private Long id;
-    @FutureOrPresent(groups = {Create.class})
-    @NotNull(groups = {Create.class})
     private LocalDateTime start;
-    @Future(groups = {Create.class})
-    @NotNull(groups = {Create.class})
     private LocalDateTime end;
     private Long itemId;
 }
--- server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoInput.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoInput.java
@@ -2,9 +2,7 @@ package ru.practicum.shareit.request.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.user.valid.Create;
 
-import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Data
@@ -13,7 +11,6 @@ public class ItemRequestDtoInput {
 
     private Long id;
     private Long requestorId;
-    @NotBlank(groups = {Create.class})
     private String description;
     private LocalDateTime created = LocalDateTime.now();
 }
